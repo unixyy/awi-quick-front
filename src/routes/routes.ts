5da887b8471e803a4f3f2acf6e0991f1ae8104eb
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-03-06 15:42:52 +0100

    fix: routes and API_URL

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
 const root = process.env.API_URL;
 
-export const gameRoot = root + "game/";
+export const gameRoot = root + "/game/";
 export const gameById = (id: string) => gameRoot + id;
 export const gamesByName = (name: string) => gameRoot + "name/" + name;
 export const gamesByType = (type: string) => gameRoot + "type/" + type;
 export const gamesZone = gameRoot + "zone/";
 export const gameByZone = (zoneId: number) =>
   gameRoot + "zone/" + zoneId;
-export const zoneRoot = root + "zone/";
+export const zoneRoot = root + "/zone/";
 export const roomRoot = zoneRoot + "room/";
 export const zoneById = (id: number) => zoneRoot + id;
 export const addRoomToZone = (id: number) => zoneById(id) + "/append";
@@ -15,11 +15,11 @@ export const removeRoomFromZone = (id: number) => zoneRoot + "room/" + id;
 export const addTableToRoom = (id: number) =>
   zoneRoot + "/room/" + id + "/append";
 export const removeTableFromRoom = (id: number) => "/table/" + id;
-export const authRoot = root + "auth/";
+export const authRoot = root + "/auth/";
 export const signInRoot = authRoot + "signin";
-export const volunteerRoot = root + "volunteer/";
+export const volunteerRoot = root + "/volunteer/";
 export const volunteerByUUID = (uuid: string) => volunteerRoot + uuid;
-export const timeslotRoot = root + "timeslot/";
+export const timeslotRoot = root + "/timeslot/";
 export const assignmentForVolunteer = (id: string) =>
   volunteerRoot + id + "/assignments";
 export const assignVolunteerToRoom = (id: string, roomId: number) =>
