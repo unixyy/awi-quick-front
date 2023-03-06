90b38f28165d1bc24c64a3768be9ac52a89fae28
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-03-06 16:01:59 +0100

    feat: ios init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.polydev',
+  appName: 'festigames',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
